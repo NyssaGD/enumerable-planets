4f0849a20548f48196e3a3ed7ef91e82033fb52f
Nyssa <nyssagd@gmail.com>
2021-11-04 17:31:35 -0400

    "i got up to step 5"

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Data.java
@@ -0,0 +1,19 @@
+package org.launchcode.enumerableplanets.data;
+
+public class Data {
+    public enum Planets {
+        // list the planets here.
+        // Mercury, Venus, Earth, Mars, Jupiter,
+        // Saturn, Uranus, Neptune
+        // Don't forget to capitalization convention and enum
+        // syntax to separate value and end the list
+        MERCURY,
+        VENUS,
+        EARTH,
+        MARS,
+        JUPITER,
+        SATURN,
+        URANUS,
+        NEPTUNE
+    }
+}
